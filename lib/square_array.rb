fb8e41ca960df83d58e072b768cd93191d2d924c
Seth Maisel <seth.maisel@gmail.com>
2019-09-30 14:57:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(numbers)
 new_array = []
  
 while numbers [counter] do
-  new_array << counter ** 2 
+  new_array << numbers [counter] ** 2 
   counter += 1
 end
   return new_array
